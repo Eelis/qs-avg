ffac6f297a67e4c44e0346ca5398156f71d33fb9
Eelis van der Weegen <eelis@eelis.net>
2009-02-24 15:45:09 +0100

    Add paper/README.txt.
    Ignore-this: 2a8a02bd26f03c43b5b69b16be8535bf
    
    darcs-hash:20090224144509-bab43-06a23d1a91d745f7c5e656603ca91cb0ccc0aeeb.gz

--- scripts/make_tarball.sh
+++ scripts/make_tarball.sh
@@ -2,4 +2,4 @@
 set -ev
 cd ..
 rm -f qs-avg.tar.gz
-tar -czf qs-avg.tar.gz qs-avg/src/{*.v,README.html,SConstruct} qs-avg/paper/{abstract.ltx,compile.sh,llncs.cls,macros.sty,paper.bib,paper.pdf,paper.tex,processed.tex,splncs.bst}
+tar -czf qs-avg.tar.gz qs-avg/src/{*.v,README.html,SConstruct} qs-avg/paper/{README.txt,abstract.ltx,compile.sh,llncs.cls,macros.sty,paper.bib,paper.pdf,paper.tex,processed.tex,splncs.bst}
