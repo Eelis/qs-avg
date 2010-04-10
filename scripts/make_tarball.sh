443d0314b72b32ff34e37af4054fb507663cb44a
Eelis van der Weegen <eelis@eelis.net>
2010-04-10 14:48:08 +0200

    Minor repo reorganization. Remove website from repo.
    Ignore-this: 6315eb60d8bd06ea4e105667cce0f8c5
    
    darcs-hash:20100410124808-bab43-2c7f4ade6f1fd4ae62cedf7c97e058f96fabdcda.gz

--- scripts/make_tarball.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-set -ev
-cd ..
-rm -f qs-avg.tar.gz
-tar -czf qs-avg.tar.gz qs-avg/src/{*.v,README.html,SConstruct} qs-avg/paper/{README.txt,abstract.ltx,compile.sh,llncs.cls,macros.sty,paper.bib,paper.pdf,paper.tex,processed.tex,splncs.bst}
